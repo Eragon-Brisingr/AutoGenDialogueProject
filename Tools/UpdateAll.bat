7c5d819b42c4a6f7f4d7637be5285f5f30a13ed6
Eragon <450614754@qq.com>
2019-11-13 09:43:51 +0800

    修改子模块路径

--- Tools/UpdateAll.bat
+++ Tools/UpdateAll.bat
@@ -1,6 +1,7 @@
+REM ## git sync
 git pull
-git submodule sync
-git submodule init
-git submodule update -f
 git submodule foreach "git checkout master"
 git submodule foreach "git pull"
+
+REM ## generate visual studio project
+GenerateProjectFiles.bat ../AutoGenDialogueP.uproject
